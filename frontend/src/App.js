4c9a9caf68c72170f06309b20b221e49af11801b
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2020-05-01 19:00:16 -0300

    frontend feito

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 import './global.css'
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
